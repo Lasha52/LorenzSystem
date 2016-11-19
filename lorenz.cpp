a314277159818bc2781e01680b62f94b73541aed
lasha52 <lashajibgashvili@gmail.com>
2016-11-19 19:13:57 +0400

    much better file saving

--- lorenz.cpp
+++ lorenz.cpp
@@ -10,11 +10,7 @@ using namespace sf;
 int main(int argc,char *argv[]){
 	float x,y,z,k1,k2,k3;
 	float dx,dy,dz,dt;
-	float resultsX[10000];
-	float resultsY[10000];
-	float resultsZ[10000];
-	int i = 0;
-	int j = 0; 
+
 	dt=0.001;
 	//just checking arguments from the console
 	if (std::string(argv[1])=="-help"){
@@ -34,9 +30,11 @@ int main(int argc,char *argv[]){
 	std::cin>>x>>y>>z>>k1>>k2>>k3;
 	}
 	RenderWindow window(VideoMode(1000, 800), "Lorenz System"); //making a window with 1000x800 resolution
-	RectangleShape pixel;//making a rectangle with (1,1) size will serve as a pixel
+	RectangleShape pixel;//making a rectangle with (1,1) size, will serve as a pixel
 	pixel.setSize(Vector2f(1,1));
 	pixel.setFillColor(Color::Green);
+	std::ofstream results;
+	results.open("results.txt");
 	while (window.isOpen()){
 		Event event;
 		
@@ -45,7 +43,7 @@ int main(int argc,char *argv[]){
 				break;	
 		}//we write this so we can quit the program when we close the window
 	
-	dx=(k1*(y-x))*dt;//equations in differential form, initially equations are defined as derivatives we don't want that we want changes in every direction for some fixed dt
+	dx=(k1*(y-x))*dt;//equations in differential form, initially equations are defined as derivatives, we don't want that, we want changes in every direction for some fixed dt
 	
 	dy=(x*(k2-z)-y)*dt;
 	
@@ -53,47 +51,14 @@ int main(int argc,char *argv[]){
 	x=x+dx;
 	y=y+dy;
 	z=z+dz;
-	j++;
-	if (j>100){//saving data in arrays every 100th iteration so we can write them to files
-	resultsX[i]=x;
-	resultsY[i]=y;
-	resultsZ[i]=z;
-	i++;
-	j=0;
-	}
+	results<<x<<" "<<y<<" "<< z<<std::endl;//writing to the file results.txt
 	std::cout<<x<<" "<<y<<" "<<z<<std::endl;
 	pixel.setPosition(5*x+400,5*y+400);//multiplying by 5 just scales the image, +400 calibrates it to the center
 	window.draw(pixel);//drawing the pixel for current iteration
 	window.display();//displaying the still for the current iteration
 	// window.clear(); //notice we dont use window.clear()? if we did, we would just see the point moving along the curve as we would redraw the point for every iteration.
 	}
-	std::ofstream resultx;
-    resultx.open ("resultx.txt");
-    j=0;
-   while(j<10000){
-   	resultx<< resultsX[j];
-   	resultx<< " ";
-  	j++;
-  	}
-  	resultx.close();
-  	std::ofstream resulty;
-    resulty.open ("resulty.txt");
-    j=0;
-   while(j<10000){
-   	resulty<< resultsY[j];
-   	resulty<< " ";
-  	j++;
-  	}
-  	resulty.close();
-  	std::ofstream resultz;
-    resultz.open ("resultz.txt");
-    j=0;
-   while(j<10000){
-   	resultz<< resultsZ[j];
-   	resultz<< " ";
-  	j++;
-  	}
-  	resultz.close();
+	results.close();
   	window.close();
   	return 0;
 }
